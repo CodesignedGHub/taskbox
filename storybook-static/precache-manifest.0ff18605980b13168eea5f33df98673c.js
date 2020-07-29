e6addbe602de4de78343d43b91640b46956e690d
Ryan Boss <rboss@codesigned.com>
2020-07-29 15:45:47 -0400

    taskbox UI

---
+++ storybook-static/precache-manifest.0ff18605980b13168eea5f33df98673c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "62b651472cac34300d7f3dcb3328d6c9",
+    "url": "iframe.html"
+  },
+  {
+    "url": "main.9b8c4a772c4067c20bd9.bundle.js"
+  },
+  {
+    "url": "runtime~main.9b8c4a772c4067c20bd9.bundle.js"
+  },
+  {
+    "revision": "ecb15bf371305c34422f",
+    "url": "static/css/main.67471bcc.chunk.css"
+  },
+  {
+    "url": "vendors~main.9b8c4a772c4067c20bd9.bundle.js"
+  },
+  {
+    "url": "vendors~main.9b8c4a772c4067c20bd9.bundle.js.LICENSE.txt"
+  }
+]);
